5ed2d34c982f5dc310a458a2b241eac4f2f33ccb
Efrain <efrender@hotmail.com>
2022-07-23 18:39:10 -0400

    slider

--- js/app.js
+++ js/app.js
@@ -1,12 +1,51 @@
 //Variables
-const toggleMenu = document.getElementById('btn-menu');
-const menu = document.querySelector('.menu');
+const images = [...(document.querySelectorAll('.img-slider'))];
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const sliderLength = images.length;
 
 //Event Listener
-toggleMenu.addEventListener('click', burgerMenu);
+prev.addEventListener('click', prevSlider);
+next.addEventListener('click', nextSlider);
 
 //Funtions
-function burgerMenu(e){
-   menu.classList.toggle('menu-open');
 
+/** Función slider
+ * Funcion que remueve la clase 'active' y la reasigna a la foto anterior
+ *  o siguiente del slider, según el botón que se pulse
+ */
+function slider(imageIndex){
+  for(slides of images){
+    slides.classList.remove('active');
+    images[imageIndex].classList.add('active');
+  } 
+  
+}
+
+// Función prevSlider() busca el indice de la foto que antecede a la actual
+ 
+function prevSlider(){
+  const index = images.findIndex(find => find.classList.contains('active'));
+ 
+    if (index > 0) {
+      prevImageIndex = index -1;     
+    }else{
+      prevImageIndex = sliderLength -1;
+    }
+    slider(prevImageIndex);
+ 
+}
+// Función prevSlider() busca el indice de la foto que le sigue a la actual
+ 
+function nextSlider(){
+  const index = images.findIndex(find => find.classList.contains('active'));
+    if (index == sliderLength - 1 ) {
+      nextImageIndex = 0;
+      slider(nextImageIndex);       
+    }else{
+      nextImageIndex = index + 1;
+      slider(nextImageIndex);
+    }
+   
+ 
 }
